90824584ac624c419d768bb0e8798599889ba0d3
MohanRaj <mraj86698@gmail.com>
2022-07-26 22:12:15 +0530

    Added Queue Operations for addfront,addrear,deleterear,deletefront,peek,checkEmpty,size

--- src/Queue/Queue.java
+++ src/Queue/Queue.java
@@ -1,5 +1,7 @@
 package Queue;
 
+import java.util.NoSuchElementException;
+
 public class Queue<T extends Comparable<T>> {
 	Node<T> head;
 	Node<T> tail;
@@ -59,6 +61,86 @@ public class Queue<T extends Comparable<T>> {
 	public boolean isEmpty() {
 		return head == null ? true : false;
 	}
+
+	/**
+	 * @purpose adds a new item at front in DeQue
+	 * @param item it needs a item
+	 * @return it returns nothing
+	 */
+	public void addFront(T item) {
+		Node<T> node = new Node<T>(item);
+		node.next = head;
+		head = node;
+
+	}
+
+	/**
+	 * @purpose adds a new item at rear in DeQue
+	 * @param item it needs a item
+	 * @return it returns nothing
+	 */
+	public void addRear(T item) {
+		enqueue(item);
+	}
+
+	/**
+	 * @purpose removes the top item from the queue
+	 * @param it needs nothing
+	 * @return returns the first item
+	 */
+
+	public T removeFront() {
+		T data = dequeue();
+		return data;
+	}
+
+	/**
+	 * @purpose removes the rear item from the queue
+	 * @param it needs nothing
+	 * @return returns the rear item
+	 */
+	public T removeRear() {
+		T data = null;
+		Node<T> temp = head;
+		if (head.next == null) {
+			data = head.data;
+			head = null;
+			return data;
+		}
+		while (temp.next.next != null) {
+			temp = temp.next;
+		}
+
+		data = temp.next.data;
+		temp.next = null;
+		return data;
+	}
+
+	/**
+	    * Peek: Return the top element of the queue without deleting it.
+	    * @return
+	    */
+	public T peek() {
+		if (isEmpty() )
+            throw new NoSuchElementException("Underflow Exception") ;
+        return head.getData();
+	}
+
+	/**
+	 * @purpose checks the size of the queue
+	 * @param it needs nothing
+	 * @return it returns INTEGER to show size
+	 */
+	public int size() {
+		Node<T> temp = head;
+		int count = 0;
+		while (temp != null) {
+			count++;
+			temp = temp.next;
+		}
+		return count;
+	}
+
 
 
 
--- src/Queue/QueueMain.java
+++ src/Queue/QueueMain.java
@@ -15,7 +15,13 @@ public class QueueMain {
 			System.out.println("1. Enqueue");
 			System.out.println("2. Dequeue");
 			System.out.println("3. Display");
-
+			System.out.println("4. AddFront");
+			System.out.println("5. AddRear");
+			System.out.println("6. Remove Front");
+			System.out.println("7. Remove Rear");
+			System.out.println("8. peek");
+			System.out.println("9. check empty");
+			System.out.println("10. size");
 			System.out.println("Enter Your Choice:");
 			int choice1 = Utility.inputInteger();
 			switch (choice1) {
@@ -39,7 +45,48 @@ public class QueueMain {
 				queue.display();
 				break;
 
+			case 4:
+				System.out.println("How many items do u want to insert into the queue: ");
+				int totalItem3 = Utility.inputInteger();
+				System.out.println("Enter element to insert");
+				for (int i = 0; i < totalItem3; i++) {
+					int item = Utility.inputInteger();
+					queue.addFront(item);
+				}
+				System.out.println("Inserted an element to the top of the queue.");
+				break;
+			case 5:
+				System.out.println("How many items do u want to insert into the queue: ");
+				int totalItem4 = Utility.inputInteger();
+				System.out.println("Enter element to insert");
+				for (int i = 0; i < totalItem4; i++) {
+					int item = Utility.inputInteger();
+					queue.addRear(item);
+				}
+				System.out.println("Inserted an element to the end of the queue.");
+				break;
+			case 6:
+				queue.removeFront();
+				System.out.println("Removed Front Element");
 
+				break;
+			case 7:
+				queue.removeRear();
+				System.out.println("Removed Last Element");
+				break;
+			case 8:
+
+				 System.out.println("Peek Element = " + queue.peek());
+				break;
+
+			case 9:
+				System.out.println("Empty status = " + queue.isEmpty());
+				break;
+			case 10:
+				System.out.println("Size = " + queue.size());
+				System.out.println("Queue = ");
+				queue.display();
+				break;
 			default:
 				System.out.println("Wrong Entry \n ");
 				break;
